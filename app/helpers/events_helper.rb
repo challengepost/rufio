f94cccfa41fa6e6f2818912d9a40ef2c39dac40d
Kevin Bongart <contact@kevinbongart.net>
2013-01-29 18:23:31 -0500

    Add links on customers to their Customer.io profile.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def customerio_base_url
+    'https://app.customer.io'
+  end
+
+  def customer_url(customer_id)
+    "#{customerio_base_url}/customers/#{customer_id}"
+  end
 end
